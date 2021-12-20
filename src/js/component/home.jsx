d51c24d46bd6df2a12dc4b56316242c3065ed87c
jbara55 <barahona.jorge@gmail.com>
2021-12-20 14:08:28 +0000

    parte1

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,80 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+import { FaRegTrashAlt } from "react-icons/fa";
+import { fFaPlusrom } from "react-icons/fa";
+
 //create your first component
 const Home = () => {
+	//hooks
+	const [tareas, setTareas] = useState("");
+	const [listaTareas, setListatareas] = useState([]);
+	useEffect(() => {
+		console.log(tareas);
+	});
+
+	const validatetareas = () => {
+		if (tareas === "") {
+			console.log("El campo tarea no debe estar vacio");
+		} else {
+			console.log("perfect!");
+		}
+
+		setTareas("");
+	};
+
+	let arreglo = [];
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="text-center mt-5">
+				<h2>To Do List</h2>
+				<input
+					type="text"
+					placeholder="Nombre de tarea"
+					onChange={(e) => setTareas(e.target.value)}
+					value={tareas}
+				/>
+				<button
+					className="btn btn-sm btn-success float-right mx-2"
+					type="submit"
+					onClick={() => {
+						validatetareas();
+						setListatareas([...listaTareas, tareas]);
+					}}>
+					Agregar
+				</button>
+				<div className="container ">
+					<ul className="list-group list-group-flush ">
+						{listaTareas.map((item, index) => {
+							return (
+								<li
+									className="list-group-item  d-flex w-100 justify-content-between"
+									key={index}>
+									{item}
+									<button
+										className="btn btn-danger"
+										type="button"
+										onClick={() => {
+											const listaNueva =
+												listaTareas.filter(
+													(key) => key !== item
+												);
+											setListatareas(listaNueva);
+											console.log(listaNueva);
+											console.log("hola");
+										}}>
+										Eliminar
+									</button>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+			</div>
+		</>
 	);
 };
 
